8156ac785bbbd0a560a1933295054b00833a89b2
AndersonLoop <coellojanderson@gmail.com>
2026-03-07 10:22:17 -0500

    Actualizar el puerto y la clase principal para ejecutar por consola

--- src/main/java/AndersonC15/Application.java
+++ src/main/java/AndersonC15/Application.java
@@ -1,13 +1,21 @@
 package AndersonC15;
 
+import AndersonC15.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	@Bean
+	public CommandLineRunner run(Principal principal) {
+		return args -> principal.mostrarMenu();
 	}
 
 }
